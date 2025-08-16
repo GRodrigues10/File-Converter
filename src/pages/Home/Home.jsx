3861dd7f9478d90ab24301cb6b2dc1f427f53faf
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-08-15 21:43:18 -0300

    atualização do main

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Main from "../../components/main/Main";
+import Main from "../../components/Main/Main";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 
