f6a62cf2960cde35e7549189b87387a81081ca7a
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-08-15 21:38:07 -0300

    atualização arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './pages/home/Home'
-import About from './pages/about/About'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import About from "./pages/About/About";
 
 
 function App() {
-
-
   return (
     <>
-     <Router basename='/File-Converter/'>
+      <Router basename="/File-Converter/">
         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
-     </Router>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
