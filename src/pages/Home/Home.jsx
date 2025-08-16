bed872871feba3ebdb3ca1d37c21f450ff994e2e
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-08-15 21:45:04 -0300

    atualização do footer e do header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import Main from "../../components/Main/Main";
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
 
 function Home() {
   return (
