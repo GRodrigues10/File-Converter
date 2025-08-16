48bc8f93c2de25d9454e15e78eb5d5f0736a45d3
Gabriel <gabriel.rodrigues.costa2001@gmail.com>
2025-08-15 21:18:32 -0300

    atualização do gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-     <Router>
+     <Router basename='/File-Converter/'>
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
